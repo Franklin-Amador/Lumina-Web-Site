028a88e158ff2d152c89e6668255997a703af92a
Franklin Amador <joelamador55@gmail.com>
2024-06-18 21:06:54 -0600

    avance 2, colores paleta de colores

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,17 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			// ? Paleta de colores
+			colors: {
+				'custom-1' : '#071739',
+				'custom-2' : '#4B6382',
+				'custom-3' : '#A4B5C4',
+				'custom-4' : '#CDD5DB',
+				'custom-5' : '#A68868',
+				'custom-6' : '#E3C39D',
+			}			
+		},
 	},
 	plugins: [],
 }
