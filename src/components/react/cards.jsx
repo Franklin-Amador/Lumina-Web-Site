05370615918d70bcf19b6728980391810c25a57f
Franklin Amador <joelamador55@gmail.com>
2024-06-20 11:33:12 -0600

    mejoras de calidad

--- src/components/react/cards.jsx
+++ src/components/react/cards.jsx
@@ -1,22 +1,28 @@
 const Card = ({ title, description, image, topic, route }) => {
     return (
-     
-<article className="overflow-hidden rounded-lg shadow transition hover:shadow-lg w-72 my-2">
+    
+    
+<article className="overflow-hidden rounded-lg shadow transition hover:shadow-lg w-72 my-2" >
+<a href={route}>
 <img
   alt={title}
   src={image}
   className="h-56 w-full object-cover"
+  
 />
+</a>
 
+<a href={route}>
 <div className="bg-white p-4 sm:p-6">
-  <a href={route}>
-    <h3 className="mt-0.5 text-lg text-gray-900">{topic}</h3>
-  </a>
+ 
+ <h3 className="mt-0.5 text-lg text-gray-900">{topic}</h3>
 
-  <p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-500">
-   {description}
-  </p>
+<p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-500">
+{description}
+</p>
 </div>
+</a>
+
 </article>
     );
 };
